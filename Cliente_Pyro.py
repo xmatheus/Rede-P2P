7a34f584fb311a2a51e0d9d0ea7b05fdd526c467
Matheus Felipe <matheuscorreia559@gmail.com>
2021-03-31 22:21:02 -0400

    feat: add option to send file and execute the load balance

--- Cliente_Pyro.py
+++ Cliente_Pyro.py
@@ -2,12 +2,13 @@ import Pyro5.api
 import uuid
 import time
 import threading
+import os
 
 from colors import bcolors
 
 id = uuid.uuid4()
 banner = ("""\
-██████╗ ██████╗ ██████╗     ████████╗██████╗  █████╗ ██████╗  █████╗ ██╗     ██╗  ██╗ ██████╗ 
+██████╗ ██████╗ ██████╗     ████████╗██████╗  █████╗ ██████╗  █████╗ ██╗     ██╗  ██╗ ██████╗
 ██╔══██╗╚════██╗██╔══██╗    ╚══██╔══╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗██║     ██║  ██║██╔═══██╗
 ██████╔╝ █████╔╝██████╔╝       ██║   ██████╔╝███████║██████╔╝███████║██║     ███████║██║   ██║
 ██╔═══╝ ██╔═══╝ ██╔═══╝        ██║   ██╔══██╗██╔══██║██╔══██╗██╔══██║██║     ██╔══██║██║   ██║
@@ -17,8 +18,13 @@ banner = ("""\
 
 @ Pyro5.api.expose
 class GlobalFunctions(object):
-    def test(self):
-        print('Teste -> ' + id)
+    def saveFile(self, name, content):
+        try:
+            with open('./user-files/'+name, 'a') as output:
+                output.write(content)
+            return True
+        except:
+            return False
 
 
 class myThread (threading.Thread):
@@ -34,41 +40,66 @@ class myThread (threading.Thread):
         ns = Pyro5.api.locate_ns()               # encontra o servidor de nomes
         uri = daemon.register(GlobalFunctions)   # Registra como um objeto Pyro
         # Registra um objeto com um nome no servidor de nomes
-        ns.register(id, uri)
+        ns.register(str(id), uri)
         daemon.requestLoop()                     # Começa o loop para esperar chamadas
 
 
+def loadFiles():
+    if not os.path.exists('user-files'):
+        os.makedirs('user-files')
+
+    arr = os.listdir('./user-files/')
+    return arr
+
+
+localFiles = loadFiles()
+
 adder = Pyro5.api.Proxy("PYRONAME:Main")
-adder.connect(id, ['0.txt', '1.txt'])
+adder.connect(id, localFiles)
 
 
 try:
     thread1 = myThread()
     thread1.start()
 except:
-    print(bcolors.FAIL+"[Error] Unable to start thread"+bcolors.ENDC)
+    print(bcolors.FAIL+"[Error] não deu para iniciar uma thread"+bcolors.ENDC)
 
 try:
     option = -1
     print("\n"+banner)
     while(option != 0):
         option = int(input(
-            "\n"+bcolors.OKCYAN+"1 - listar todos os arquivos\n2 - Enviar arquivo\n3 - Teste\n"+bcolors.WARNING+"0 - Sair\n\n"+bcolors.ENDC+"Digite: "))
+            "\n"+bcolors.OKCYAN+"1 - listar todos os arquivos\n2 - Enviar arquivo\n3 - Teste\n"+bcolors.WARNING+"0 - Sair\n\n"+bcolors.ENDC+"Opção: "))
         if(option == 1):
-            print(bcolors.OKGREEN+"\n[Arquivos disponíveis]"+bcolors.ENDC)
-            print(adder.listFiles(id))
+            files = adder.listFiles(id)
+            print(bcolors.OKGREEN +
+                  "\n[Arquivos disponíveis] => "+bcolors.ENDC+str(len(files)))
+            print(files)
+
         elif(option == 2):
-            print(option)
+            fileName = input("Nome do arquivo: ")
+            content = input("Conteúdo: ")
+            adder.sendFile(fileName, content)
+
         elif(option == 3):
             print(option)
+
         elif(option == 0):
             print(bcolors.OKGREEN + "\n[!] Bye"+bcolors.ENDC)
+
         else:
             print(bcolors.FAIL +
                   "\n[!] Essa ai a gente não implementou :|"+bcolors.ENDC)
+
     print('\n'+bcolors.WARNING+'[INFO] '+bcolors.ENDC+'Press ctrl+c')
-except:
-    adder.closeConnection(id)
+
+except KeyboardInterrupt:
+    print(bcolors.OKGREEN+"\n[!] Bye"+bcolors.ENDC)
+    print('\n'+bcolors.WARNING+'[INFO] '+bcolors.ENDC+'Press ctrl+c')
+
+except Exception as error:
+    print(bcolors.FAIL+"[Error] falha no menu"+bcolors.ENDC)
+    print(error)
 
 adder.closeConnection(id)
 exit(0)
--- Servidor_Pyro.py
+++ Servidor_Pyro.py
@@ -1,5 +1,7 @@
 import Pyro5.api
 from colors import bcolors
+from datetime import datetime
+import uuid
 # servidor de nomes
 # '~/.local/bin/pyro5-ns'
 
@@ -14,11 +16,20 @@ class Client:
     def getFiles(self):
         return self.files
 
+    def saveFile(self):
+        pass
+
 
 def deleteClient(id):
     for client in clients:
         if(client.id == id):
             clients.remove(client)
+
+
+def getFormatedTime():
+    now = datetime.now()
+    return ('%02d:%02d.%d' %
+            (now.minute, now.second, now.microsecond))[:-4]
 
 
 @Pyro5.api.expose
@@ -27,19 +38,47 @@ class GlobalFunctions(object):
     def connect(self, id, files):
         clients.append(Client(id, files))
 
-        print(bcolors.OKGREEN+"[  CONNECT ]\t"+bcolors.ENDC+id)
+        print(bcolors.OKGREEN+"[  CONNECT ]" +
+              bcolors.OKCYAN+"["+getFormatedTime()+"]\t"+bcolors.ENDC+id)
 
     def closeConnection(self, id):
         deleteClient(id)
-        print(bcolors.WARNING+"[DISCONNECT]\t"+bcolors.ENDC+id)
+
+        print(bcolors.WARNING+"[DISCONNECT]" +
+              bcolors.OKCYAN+"["+getFormatedTime()+"]"+"\t"+bcolors.ENDC+id)
 
     def listFiles(self, id):
         files = []
         for client in clients:
             files.append(client.getFiles())
         return [item for sublist in files for item in sublist]
-        # proxyClient = Pyro5.api.Proxy("PYRONAME:"+id)
-        # proxyClient.test(files)
+
+    def sendFile(self, name, content):
+        print(bcolors.OKBLUE+"[RECEIVING-FILE]\t"+bcolors.ENDC+name)
+
+        fileId = uuid.uuid4()
+        client = self.loadBalacing()
+
+        proxyClient = Pyro5.api.Proxy("PYRONAME:"+str(client.id))
+        fileName = str(fileId)+"-"+name
+        response = proxyClient.saveFile(fileName, content)
+
+        if(response == True):
+            client.files.append(fileName)
+            print(bcolors.OKBLUE+"[  SAVED-FILE  ]\t"+bcolors.ENDC +
+                  name+" IN "+bcolors.WARNING+client.id+bcolors.ENDC)
+        else:
+            print(bcolors.FAIL+"[ UNSAVED-FILE ]\t"+bcolors.ENDC +
+                  name+" IN "+bcolors.WARNING+client.id+bcolors.ENDC)
+
+    def loadBalacing(self):
+        selected = clients[0]
+
+        for client in clients:
+            if(len(client.files) < len(selected.files)):
+                selected = client
+
+        return selected
 
 
 daemon = Pyro5.server.Daemon()           # cria um daemon
